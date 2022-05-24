eff83e0917a3d4950aa87f52f9a2c18b9ca769bc
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-05-24 11:23:16 +0300

    SASS base files

--- kyky/src/index.js
+++ kyky/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import ReactDOM from 'react-dom/client';
 import './index.css';
+=======
+import ReactDOM from 'react-dom';
+import './styles/index.scss';
+>>>>>>> Stashed changes
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +13,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+<<<<<<< Updated upstream
   </React.StrictMode>
+=======
+  </React.StrictMode>,
+  document.getElementById('root')
+>>>>>>> Stashed changes
 );
 
 // If you want to start measuring performance in your app, pass a function
