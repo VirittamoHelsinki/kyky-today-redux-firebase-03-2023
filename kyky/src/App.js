7a06c2ef4e0ea1904ccb5160be612cd7b59f1152
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-05-24 11:24:18 +0300

    styles-sandbox

--- kyky/src/App.js
+++ kyky/src/App.js
@@ -1,7 +1,11 @@
+import StyleSandbox from './StyleSandbox';
 
 function App() {
   return (
+    <>
       <div className="App">Hello, World</div>
+      <StyleSandbox />
+    </>
   );
 }
 
---
+++ kyky/src/StyleSandbox.js
@@ -0,0 +1,13 @@
+// For visual component guidance and testing :)
+
+const StyleSandbox = () => {
+  return (
+    <div>
+      <h1>H1 title</h1>
+      <h2>H2 title</h2>
+      <button>Primary button</button>
+    </div>
+  );
+};
+
+export default StyleSandbox;
