2961653b3a31d0a8f246b1d920180135acdfeaab
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-05-25 13:01:14 +0300

    lots of style fixes

--- kyky/src/StyleSandbox.js
+++ kyky/src/StyleSandbox.js
@@ -5,10 +5,30 @@ import Language from './language';
 function StyleSandbox() {
   const { lang } = useContext(Language);
   return (
-    <div>
+    <div className="sandbox">
+      <h1>Style SandBox</h1>
       <h1>{lang.h1_title}</h1>
       <h2>{lang.h2_title}</h2>
       <button type="button">{lang.primary_button}</button>
+      <button type="button" className="button-primary">
+        Primary button
+      </button>
+      <form className="card-light">
+        <h1>Title</h1>
+        <label htmlFor="test">Label text</label>
+        <input
+          type="text"
+          id="text-input-test"
+          name="text-input-test"
+          placeholder="Placeholder text"
+        />
+        <div className="checkbox-container">
+          <input type="checkbox" id="checkbox-test" name="checkbox-test" value="test" />
+          <label className="checkbox-label" htmlFor="checkbox-test">
+            Checkbox Label
+          </label>
+        </div>
+      </form>
     </div>
   );
 }
