3f58420080abb80e7a60764d8776740189101dfd
juhana <61124548+WelehoBRUDER@users.noreply.github.com>
2022-05-25 09:56:25 +0300

    Added language context

--- kyky/src/StyleSandbox.js
+++ kyky/src/StyleSandbox.js
@@ -1,11 +1,14 @@
 // For visual component guidance and testing :)
+import { useContext } from 'react';
+import Language from './language';
 
 function StyleSandbox() {
+  const { lang } = useContext(Language);
   return (
     <div>
-      <h1>H1 title</h1>
-      <h2>H2 title</h2>
-      <button type="button">Primary button</button>
+      <h1>{lang.h1_title}</h1>
+      <h2>{lang.h2_title}</h2>
+      <button type="button">{lang.primary_button}</button>
     </div>
   );
 }
