3a36c0924c9da8937bdeb8dabcf42bd4248278c8
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-05-25 14:39:19 +0300

    localisation refactoring

--- kyky/src/StyleSandbox.js
+++ kyky/src/StyleSandbox.js
@@ -5,32 +5,33 @@ import Language from './language';
 function StyleSandbox() {
   const { lang } = useContext(Language);
   return (
-    <div className="sandbox">
+    <main className="sandbox">
       <h1>Style SandBox</h1>
       <br />
-      <h1>{lang.sandbox_h1}</h1>
-      <h2>{lang.sandbox_h2}</h2>
-      <h3>{lang.sandbox_h3}</h3>
-      <button type="button" className="button-primary">
-        {lang.sandbox_button_primary}
-      </button>
-      <form className="card-light">
-        <h1>{lang.sandbox_card_title}</h1>
-        <label htmlFor="test">{lang.sandbox_text_label}</label>
+      <h1>{lang.sandbox.h1}</h1>
+      <h2>{lang.sandbox.h2}</h2>
+      <h3>{lang.sandbox.h3}</h3>
+      <form className="card-light small">
+        <h1>{lang.sandbox.card_title}</h1>
+        <label htmlFor="test">{lang.sandbox.text_label}</label>
         <input
           type="text"
           id="text-input-test"
           name="text-input-test"
-          placeholder={lang.sandbox_placeholder}
+          placeholder={lang.sandbox.placeholder}
         />
         <div className="checkbox-container">
           <input type="checkbox" id="checkbox-test" name="checkbox-test" value="test" />
           <label className="checkbox-label" htmlFor="checkbox-test">
-            {lang.sandbox_checkbox_label}
+            {lang.sandbox.checkbox_label}
+            <a href="https://www.google.com/">{lang.sandbox.link}</a>
           </label>
         </div>
+        <button type="button" className="button-primary">
+          {lang.sandbox.button_primary}
+        </button>
       </form>
-    </div>
+    </main>
   );
 }
 
