d97e438ee66e6c7dfb351a20a9e909e8e1900595
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-05-30 11:09:10 +0300

    Revert "added UserRegistration component"
    This reverts commit bceee3fdcd47e3ab8b5b8ebde680377596031528.

--- kyky/src/App.js
+++ kyky/src/App.js
@@ -6,7 +6,6 @@ import en from './localisation/en.json';
 import StyleSandbox from './StyleSandbox';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import UserRegistration from './components/UserRegistration';
 
 const languages = { fi, en };
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Header languages={languages} lang={lang} setLang={setLang} />
         <div className="App">Hello, World</div>
         <StyleSandbox />
-        <UserRegistration />
         <Footer />
       </>
     </Language.Provider>
