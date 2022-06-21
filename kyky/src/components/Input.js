9214af4ce8e1c9be8a1bbe54e4bb7db89e6d4410
juhana <61124548+WelehoBRUDER@users.noreply.github.com>
2022-06-21 08:46:25 +0300

    added an input component
    It's most likely not optimal, but this is a first iteration after all.

---
+++ kyky/src/components/Input.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import 'material-icons/iconfont/material-icons.css';
+
+/*
+ * This is a component that renders a label and an input wrapped with a wrapper div.
+ * The wrapper div can be styled with the className prop. Defaults to "input-container"
+ * Types of input are text, password, email, number, and url.
+ * Name is used both as the "name" property of the input and as the "id" property of the label.
+ * Use the icon property if you want to add a single icon, otherwise pass them as children to the component.
+ * Children are passed to the wrapper div, use css if you must get them inside the input.
+ */
+export default function Input({
+  type,
+  name,
+  className = 'input-container',
+  placeholder = 'input',
+  autoComplete = 'off',
+  title = 'label',
+  required = false,
+  onChange = () => {},
+  value = '',
+  icon = { text: '', className: '' },
+  children
+}) {
+  return (
+    <div className={className}>
+      <label id={name} htmlFor={name}>
+        {title}
+      </label>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        required={required}
+        onChange={onChange}
+        value={value}
+      />
+      {icon.className !== '' && <i className={icon.className}>{icon.text}</i>}
+      {children}
+    </div>
+  );
+}
--- kyky/src/pages/ProfileCreation.js
+++ kyky/src/pages/ProfileCreation.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import Language from '../language';
-import 'material-icons/iconfont/material-icons.css';
 import pic from '../image/portrait-of-smiling-young-man-wearing-glasses-DIGF000310.jpg';
+import Input from '../components/Input';
 
 export default function ProfileCreation() {
   const { lang } = useContext(Language);
@@ -16,67 +16,49 @@ export default function ProfileCreation() {
           <p className="profile-pic-add-text">{lang.profile_creation.add_portrait}</p>
         </div>
         <section>
-          <div className="input-container">
-            <label htmlFor="name">{lang.profile_creation.name}</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder={lang.profile_creation.name}
-              autoComplete="username"
-            />
-            <i className="material-icons-outlined">info</i>
-          </div>
-          <div className="input-container">
-            <label htmlFor="phone">{lang.profile_creation.phone}</label>
-            <input
-              type="text"
-              id="phone"
-              name="phone"
-              placeholder={lang.profile_creation.phone}
-              autoComplete="tel"
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="address">{lang.profile_creation.address}</label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              placeholder={lang.profile_creation.address}
-              autoComplete="street-address"
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="city">{lang.profile_creation.city}</label>
-            <input
-              type="text"
-              id="city"
-              name="city"
-              placeholder={lang.profile_creation.city}
-              autoComplete="address-level2"
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="zip">{lang.profile_creation.zip}</label>
-            <input
-              type="text"
-              id="zip"
-              name="zip"
-              placeholder={lang.profile_creation.zip}
-              autoComplete="postal-code"
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="country">{lang.profile_creation.country}</label>
-            <input
-              type="text"
-              id="country"
-              name="country"
-              placeholder={lang.profile_creation.country}
-              autoComplete="country-name"
-            />
-          </div>
+          <Input
+            type="text"
+            name="name"
+            title={lang.profile_creation.name}
+            placeholder={lang.profile_creation.name}
+            autoComplete="username"
+            icon={{ text: 'info', className: 'material-icons-outlined' }}
+          />
+          <Input
+            type="text"
+            name="phone"
+            title={lang.profile_creation.phone}
+            placeholder={lang.profile_creation.phone}
+            autoComplete="tel"
+          />
+          <Input
+            type="text"
+            name="address"
+            title={lang.profile_creation.address}
+            placeholder={lang.profile_creation.address}
+            autoComplete="street-address"
+          />
+          <Input
+            type="text"
+            name="city"
+            title={lang.profile_creation.city}
+            placeholder={lang.profile_creation.city}
+            autoComplete="address-level2"
+          />
+          <Input
+            type="text"
+            name="zip"
+            title={lang.profile_creation.zip}
+            placeholder={lang.profile_creation.zip}
+            autoComplete="postal-code"
+          />
+          <Input
+            type="text"
+            name="country"
+            title={lang.profile_creation.country}
+            placeholder={lang.profile_creation.country}
+            autoComplete="country-name"
+          />
         </section>
         <section>
           <button type="submit" className="button-primary">
