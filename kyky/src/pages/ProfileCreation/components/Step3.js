d4049cdf528e6c9bd43325630dadaa7f4483d4df
Tuukka Virtanen <duukkis@gmail.com>
2022-09-16 16:55:27 +0300

    input fields for step 3 (languages)

--- kyky/src/pages/ProfileCreation/components/Step3.js
+++ kyky/src/pages/ProfileCreation/components/Step3.js
@@ -1,10 +1,16 @@
 import '../../../styles/NewProfileCreation.scss';
 import React from 'react';
+import Input from '../../../components/Input';
 
 export default function Step3(currentStep) {
   return (
     <div className="step step3">
-      Content, step 3
+      <div className="formContainer">
+        <h2>Language</h2>
+        <Input type="text" name="finnish" placeholder="My level is" label="Finnish"></Input>
+        <Input type="text" name="swedish" placeholder="My level is" label="Swedish"></Input>
+        <Input type="text" name="english" placeholder="My level is" label="English"></Input>
+      </div>
     </div>
   );
 }
