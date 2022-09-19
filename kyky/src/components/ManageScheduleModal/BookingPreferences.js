7908a35377ff42791d59d732e93b40c7ef9b9695
juhana <61124548+WelehoBRUDER@users.noreply.github.com>
2022-09-19 15:19:51 +0300

    working on booking preferences

--- kyky/src/components/ManageScheduleModal/BookingPreferences.js
+++ kyky/src/components/ManageScheduleModal/BookingPreferences.js
@@ -1,11 +1,46 @@
+import { useState } from 'react';
+import Input from '../Input';
+import Switch from 'react-switch';
+
 export default function BookingPreferences() {
+  const bufferTimes = [
+    { value: 0, text: 'None' },
+    { value: 5, text: '5 minutes' },
+    { value: 15, text: '15 minutes' },
+    { value: 30, text: '30 minutes' },
+    { value: 60, text: '1 hour' },
+    { value: 120, text: '2 hours' }
+  ];
+  const [travelTime, setTravelTime] = useState(false);
   return (
-    <div className="booking-preferences container">
-      <p>Booking Preferences</p>
-      <div className="booking-preferences select-basic">
-        <input type="checkbox" />
-        <label>Allow booking</label>
+    <>
+      <h2>Booking Preferences</h2>
+      <div className="buffer-between container">
+        <p>Buffer between bookings</p>
+        <div className="buffers">
+          <Input type="radio" name="buffer" id="none" label="">
+            <select className="short" defaultValue={15}>
+              {bufferTimes.map((time) => (
+                <option key={time.value} value={time.value}>
+                  {time.text}
+                </option>
+              ))}
+            </select>
+          </Input>
+          <Switch
+            width={32}
+            height={16}
+            handleDiameter={14}
+            name="travel-time"
+            checked={travelTime}
+            onChange={() => setTravelTime(!travelTime)}
+          />
+          <label htmlFor="travel-time">Include travel time</label>
+        </div>
+        <div className="no-buffers">
+          <Input type="radio" name="buffer" id="none" label="Allow only one booking per day" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
