f96a69522a3b167460a1e736b369d71cc6d0670f
juhana <61124548+WelehoBRUDER@users.noreply.github.com>
2022-09-20 13:57:18 +0300

    create schedule nearly finished

--- kyky/src/components/ManageScheduleModal/BookingPreferences.js
+++ kyky/src/components/ManageScheduleModal/BookingPreferences.js
@@ -11,14 +11,26 @@ export default function BookingPreferences() {
     { value: 60, text: '1 hour' },
     { value: 120, text: '2 hours' }
   ];
+  const bookingTimes = [
+    // in hours
+    { value: 0.5, text: '30 minutes' },
+    { value: 1, text: '1 hour' },
+    { value: 2, text: '2 hours' },
+    { value: 3, text: '3 hours' },
+    { value: 6, text: '6 hours' },
+    { value: 10, text: '10 hours' },
+    { value: -1, text: 'Custom' }
+  ];
   const [limitBookings, setLimitBookings] = useState(false);
   const [travelTime, setTravelTime] = useState(false);
+  const [allowOverlap, setAllowOverlap] = useState(false);
+  const [overlapAnyType, setOverlapAnyType] = useState(true);
   return (
     <>
       <h2>Booking Preferences</h2>
       <div className="buffer-between container">
         <p>Buffer between bookings</p>
-        <div className={`buffers ${!limitBookings ? 'selected' : ''}`}>
+        <div className={`buffers box ${!limitBookings ? 'selected' : ''}`}>
           <Input
             type="radio"
             name="buffer"
@@ -46,7 +58,7 @@ export default function BookingPreferences() {
             <label htmlFor="travel-time">Include travel time</label>
           </div>
         </div>
-        <div className={`limit-bookings ${limitBookings ? 'selected' : ''}`}>
+        <div className={`limit-bookings box ${limitBookings ? 'selected' : ''}`}>
           <Input
             type="radio"
             name="buffer"
@@ -54,6 +66,58 @@ export default function BookingPreferences() {
             label="Allow only one booking per day"
             checked={limitBookings}
             onChange={() => setLimitBookings(true)}></Input>
+        </div>
+      </div>
+      <div className="booking-duration container">
+        <p>Minimum duration of a booking</p>
+        <select defaultValue={1}>
+          {bookingTimes.map((time) => (
+            <option key={time.value} value={time.value}>
+              {time.text}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="overlapping-bookings container">
+        <p>Overlapping bookings</p>
+        <div className="allow">
+          <Switch
+            width={32}
+            height={16}
+            handleDiameter={14}
+            name="allow-overlap"
+            checked={allowOverlap}
+            onChange={() => setAllowOverlap(!allowOverlap)}
+          />
+          <label htmlFor="allow-overlap">Allow other bookings to overlap with this job</label>
+        </div>
+        <div
+          className={`${allowOverlap ? '' : 'disabled'}`}
+          style={{ display: 'flex', gap: '2rem' }}>
+          <div className={`any-job box ${overlapAnyType ? 'selected' : ''}`}>
+            <Input
+              type="radio"
+              name="job-type"
+              id="any-type"
+              label="Any job type"
+              checked={overlapAnyType}
+              onChange={() => {
+                setOverlapAnyType(true);
+                setAllowOverlap(true);
+              }}></Input>
+          </div>
+          <div className={`same-job box ${!overlapAnyType ? 'selected' : ''}`}>
+            <Input
+              type="radio"
+              name="job-type"
+              id="same-type"
+              label="Same job type only"
+              checked={!overlapAnyType}
+              onChange={() => {
+                setOverlapAnyType(false);
+                setAllowOverlap(true);
+              }}></Input>
+          </div>
         </div>
       </div>
     </>
--- kyky/src/components/ManageScheduleModal/DateAndTime.js
+++ kyky/src/components/ManageScheduleModal/DateAndTime.js
@@ -13,6 +13,7 @@ export default function DateAndTime() {
     { value: 6, text: '6 months' },
     { value: 12, text: '12 months' }
   ];
+  const [schedule, setSchedule] = useState(false);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   return (
@@ -20,29 +21,45 @@ export default function DateAndTime() {
       <h2>Date & Time</h2>
       <div className="schedule-duration container">
         <p>Schedule duration</p>
-        <Input type="radio" name="auto" label="" />
-        <select className="short" defaultValue={3}>
-          {advanceTimes.map((time) => (
-            <option key={time.value} value={time.value}>
-              {time.text}
-            </option>
-          ))}
-        </select>
-        <Input type="radio" name="auto" label="" />
-        <Input
-          type="date"
-          value={startDate.toLocaleDateString('sv-SV')}
-          name="start-date"
-          label="Start date"
-          labelOnFront
-        />
-        <Input
-          type="date"
-          value={endDate.toLocaleDateString('sv-SV')}
-          name="end-date"
-          label="End date"
-          labelOnFront
-        />
+        <div className={`box center ${!schedule ? 'selected' : ''}`}>
+          <Input
+            type="radio"
+            name="auto"
+            label=""
+            checked={!schedule}
+            onChange={() => setSchedule(false)}
+          />
+          <select className="short" defaultValue={3}>
+            {advanceTimes.map((time) => (
+              <option key={time.value} value={time.value}>
+                {time.text}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={`box center ${schedule ? 'selected' : ''}`}>
+          <Input
+            type="radio"
+            name="auto"
+            label=""
+            checked={schedule}
+            onChange={() => setSchedule(true)}
+          />
+          <Input
+            type="date"
+            value={startDate.toLocaleDateString('sv-SV')}
+            name="start-date"
+            label="Start date"
+            labelOnFront
+          />
+          <Input
+            type="date"
+            value={endDate.toLocaleDateString('sv-SV')}
+            name="end-date"
+            label="End date"
+            labelOnFront
+          />
+        </div>
       </div>
       <div className="time container">
         <p>Time</p>
