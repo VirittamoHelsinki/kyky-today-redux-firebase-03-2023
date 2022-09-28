a0d90c2bed04232ed1797e2f4f0120429fe23b42
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-09-28 10:55:30 +0300

    Step 4 & 5 created

--- kyky/src/pages/ProfileCreation/GetStarted.js
+++ kyky/src/pages/ProfileCreation/GetStarted.js
@@ -1,6 +1,8 @@
 import Step1 from './components/Step1';
 import Step2 from './components/Step2';
 import Step3 from './components/Step3';
+import Step4 from './components/Step4';
+import Step5 from './components/Step5';
 import '../../styles/NewProfileCreation.scss';
 import { useState } from 'react';
 
@@ -8,16 +10,20 @@ export default function GetStarted() {
   const titles = [
     'Please add a title about what you do.',
     'If you have relevant work and/or education experience, add it here.',
-    'Good. Now tell us which languages you speak.'
+    'Good. Now tell us which languages you speak.',
+    'Almost there! What work do you do here?',
+    'What is the main service you offer?'
   ];
 
   const tips = [
     "It's the very first thing clients see. Please describe your skills with your own words.",
     'Relevant working experience will increase the chance of getting hired!',
-    'Your language skill can make client know you better.'
+    'Your language skill can make client know you better.',
+    'Write down what you can offer to clients.',
+    'Choose one service that best describes the type of work you do.'
   ];
 
-  const steps = 3;
+  const steps = 5;
 
   const [title, setTitle] = useState(0);
   const [tip, setTip] = useState(0);
@@ -37,6 +43,8 @@ export default function GetStarted() {
         {currentStep === 1 && <Step1 />}
         {currentStep === 2 && <Step2 />}
         {currentStep === 3 && <Step3 />}
+        {currentStep === 4 && <Step4 />}
+        {currentStep === 5 && <Step5 />}
         {currentStep < steps && (
           <button className="nextButton" onClick={nextPhase}>
             NEXT
---
+++ kyky/src/pages/ProfileCreation/components/Step5.js
@@ -0,0 +1,15 @@
+import '../../../styles/NewProfileCreation.scss';
+import Select from 'react-select';
+import React from 'react';
+
+export default function Step5(currentStep) {
+
+    const placeholder= "Search for a service"
+
+  return (
+    <div className="step step5">
+      <Select className="select-container"
+      placeholder={placeholder} />
+    </div>
+  );
+}
