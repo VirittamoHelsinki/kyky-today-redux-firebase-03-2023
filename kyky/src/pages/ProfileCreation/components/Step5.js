d65f2d791104bcc05ee5c0f8a61d7a5158e8d98f
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-09-28 14:07:04 +0300

    Added options to Step 5 select menu

--- kyky/src/pages/ProfileCreation/components/Step5.js
+++ kyky/src/pages/ProfileCreation/components/Step5.js
@@ -3,11 +3,18 @@ import Select from 'react-select';
 import React from 'react';
 
 export default function Step5(currentStep) {
+    const options = [
+        { value: 'itAndNetworking', label: 'IT & Networking' },
+        { value: 'salesAndMarketing', label: 'Sales & marketing' },
+        { value: 'writing', label: 'Writing' },
+        { value: 'webMobileAndSoftwareDev ', label: 'Web, Mobile & Software Dev' }
+      ];
+    
   const placeholder = 'Search for a service';
 
   return (
     <div className="step step5">
-      <Select className="select-container2" placeholder={placeholder} />
+      <Select className="select-container2" placeholder={placeholder} options={[...options]} />
     </div>
   );
 }
