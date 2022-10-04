f09406eaaa9b1f92ef369d2b94d786689db90824
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-10-04 09:01:26 +0300

    country list added to setp7

--- kyky/src/pages/ProfileCreation/components/Step7.js
+++ kyky/src/pages/ProfileCreation/components/Step7.js
@@ -3,9 +3,10 @@ import '../../../styles/_components.scss';
 import Select from 'react-select';
 import Input from '../../../components/Input';
 import React from 'react';
+import Countries from '../../../countries.json';
 import { ReactComponent as ProfileIcon2 } from '../../../image/profileicon2.svg';
 
-export default function Step7() {
+export default function Step7(currentStep) {
   return (
     <div className="step step7">
       <div className="detailsMainContainer">
@@ -15,7 +16,11 @@ export default function Step7() {
         </div>
         <div className="detailsContainer">
           <label>Country*</label>
-          <Select className="select-container3"></Select>
+          <Select
+            className="select-container3"
+            options={Countries}
+            getOptionLabel={(option) => option.name}
+            getOptionValue={(option) => option.code}></Select>
           <label>Street Address*(won’t show on profile)</label>
           <input className="detailsInput"></input>
           <label>City*</label>
@@ -24,8 +29,11 @@ export default function Step7() {
           <input className="detailsInputShort" type="text"></input>
           <label>Phone</label>
           <Select className="select-container4"></Select>
-          <input className="detailsInputPhone" placeholder='type your number' maxLength={13}></input>
-        </div> 
+          <input
+            className="detailsInputPhone"
+            placeholder="type your number"
+            maxLength={13}></input>
+        </div>
       </div>
     </div>
   );
