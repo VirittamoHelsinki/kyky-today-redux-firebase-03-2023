29abe5e843b2001624fbabc71c57ff060a38fb59
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-10-10 14:52:01 +0300

    Step 8 boxes

--- kyky/src/pages/ProfileCreation/components/Step8.js
+++ kyky/src/pages/ProfileCreation/components/Step8.js
@@ -1,10 +1,18 @@
 import '../../../styles/NewProfileCreation.scss';
-import '../../../styles/_components.scss';
+
 import React, { useState } from 'react';
 
 export default function Step8(currentStep){
     return(
-        <div className= "step step8"></div>
+        <div className= "step step8">
+            <div className= "previewMainContainer">
+                <div className= "previewContainer1"></div>
+                <div className= "previewContainer2"></div>
+                <div className= "previewContainer3"></div>
+                <div className= "previewContainer4"></div>
+                </div>
+
+        </div>
 
 
     )
