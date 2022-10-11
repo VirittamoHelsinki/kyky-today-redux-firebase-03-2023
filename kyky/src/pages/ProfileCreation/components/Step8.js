d84a5a419a8f533e005632af3477dcbbd54e686b
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-10-11 08:51:50 +0300

    Minor progress in Step8

--- kyky/src/pages/ProfileCreation/components/Step8.js
+++ kyky/src/pages/ProfileCreation/components/Step8.js
@@ -1,19 +1,23 @@
 import '../../../styles/NewProfileCreation.scss';
 
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function Step8(currentStep){
-    return(
-        <div className= "step step8">
-            <div className= "previewMainContainer">
-                <div className= "previewContainer1"></div>
-                <div className= "previewContainer2"></div>
-                <div className= "previewContainer3"></div>
-                <div className= "previewContainer4"></div>
-                </div>
-
+export default function Step8(currentStep) {
+  return (
+    <div className="step step8">
+      
+        <div className="previewContainer1"></div>
+        <div className="previewContainer2"></div>
+        <div className="previewContainer3">
+          {' '}
+          <h3>Work Experience</h3>
+          <p>No items to display.</p>
         </div>
-
-
-    )
+        <div className="previewContainer4">
+          <h3>Education History</h3>
+          <p>No items to display.</p>
+        </div>
+      
+    </div>
+  );
 }
