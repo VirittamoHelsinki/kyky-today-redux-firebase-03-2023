04f16958320ec6f2a8d0c0f4908edf3fb0651fa0
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-10-19 09:59:08 +0300

    Add skills added to Step 1

--- kyky/src/pages/ProfileCreation/components/Step1.js
+++ kyky/src/pages/ProfileCreation/components/Step1.js
@@ -1,15 +1,23 @@
 import '../../../styles/NewProfileCreation.scss';
+import { ReactComponent as MagGlass } from '../../../image/mag-glass.svg';
 import React from 'react';
 
 export default function Step1(currentStep) {
   return (
     <div className="step step1">
-      <input className= "getStartedInput"
+      <input
+        className="getStartedInput"
         id="titleInput"
         name="titleInput"
         placeholder="Example: Dogwalker"
-        aria-label="Please add a title about what you do"
-      ></input>
+        aria-label="Please add a title about what you do"></input>
+      <h1>Please add skills you have.</h1>
+      <div className="addSkills">
+        <input className="addSkillsInput"></input>
+        <div className="magGlass">
+          <MagGlass />
+        </div>
+      </div>
     </div>
   );
 }
