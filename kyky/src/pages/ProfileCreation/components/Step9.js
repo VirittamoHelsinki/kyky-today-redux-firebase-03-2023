14b6f59a20232abacf4981a7b1808455da3073f8
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-10-24 13:12:46 +0300

    step 9 minor adjustments

--- kyky/src/pages/ProfileCreation/components/Step9.js
+++ kyky/src/pages/ProfileCreation/components/Step9.js
@@ -7,7 +7,7 @@ export default function Step9(currentStep) {
     return(
         <div className= "step step9">
             <div classname= "mainContainer">
-                <div className= "leftContainer"><h1>Your profile has been successfully created, Firstname !</h1><Like></Like></div>
+                <div className= "leftContainer"><h1 className= "success">Your profile has been successfully created, Firstname !</h1><Like></Like></div>
                 <div className= "rightContainer"></div>
             </div>
 
