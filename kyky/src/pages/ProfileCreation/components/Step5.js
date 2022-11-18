346695629174fe3a0040fee5e68d7060228809b5
Tuukka Virtanen <duukkis@gmail.com>
2022-11-18 14:38:23 +0200

    edit step5 to use GenericSelect component instead of Select

--- kyky/src/pages/ProfileCreation/components/Step5.js
+++ kyky/src/pages/ProfileCreation/components/Step5.js
@@ -1,7 +1,7 @@
-import '../../../styles/NewProfileCreation.scss';
-import Select, { components } from 'react-select';
 import React from 'react';
-import { ReactComponent as CheckMark } from '../../../image/check-mark.svg';
+import { GenericSelect } from './Select';
+import '../../../styles/NewProfileCreation.scss';
+
 export default function Step5() {
   const options = [
     { value: 'itAndNetworking', label: 'IT & Networking' },
@@ -10,31 +10,13 @@ export default function Step5() {
     { value: 'webMobileAndSoftwareDev ', label: 'Web, Mobile & Software Dev' }
   ];
 
-  const placeholder = 'Search for a service';
-  const { Option } = components;
-  const IconOption = (props) => (
-    <Option {...props}>
-      {props.isSelected ? (
-        <div class="optionContainer">
-          <CheckMark id="checkMark" />
-          <span class="optionLabelTextBold">{props.data.label}</span>
-        </div>
-      ) : (
-        <div class="optionContainer">
-          <div id="noCheckMark"></div>
-          <span class="optionLabelText">{props.data.label}</span>
-        </div>
-      )}
-    </Option>
-  );
-
   return (
     <div className="step step5">
-      <Select
+      <GenericSelect
         className="select-container2"
-        placeholder={placeholder}
+        name="mainService"
+        placeholder="Search for a service"
         options={[...options]}
-        components={{ Option: IconOption }}
       />
     </div>
   );
