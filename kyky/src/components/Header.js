fc3dd0710c3a85ed4ec6479dd8b24690abec9533
Mikko Manninen <mannmik@digitalentshelsinki.internal>
2022-12-01 08:56:30 +0200

    google login works and log out button added to the profile pic

--- kyky/src/components/Header.js
+++ kyky/src/components/Header.js
@@ -1,45 +1,62 @@
 /* eslint-disable react/prop-types */
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logOut } from '../redux/userSlice';
 import SearchBar from './SearchBar';
 import '../styles/header.scss';
-import {ReactComponent as KykyLogo} from '../image/kykylogo.svg';
+import { ReactComponent as KykyLogo } from '../image/kykylogo.svg';
 
-const Header = ({navlinks}) => {
-    return (
-        <div id="navheader">
-            <div className="nav-title">
-                <a href="/">
-                    <KykyLogo/>
-                </a>
+const Header = ({ navlinks }) => {
+  const dispatch = useDispatch();
+
+  const onLogoutClick = () => {
+    dispatch(logOut());
+  };
+
+  return (
+    <div id="navheader">
+      <div className="nav-title">
+        <a href="/">
+          <KykyLogo />
+        </a>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <SearchBar />
+          </li>
+
+          <li>
+            <Link to="/user-registration">Rekisteröidy</Link>
+          </li>
+
+          <li className="dropdown">
+            <span className="material-icons-outlined">menu</span>
+            <div className="dropdown-content">
+              {navlinks.map(({ to, label }) => (
+                <div key={to}>
+                  <Link to={to}>{label}</Link>
+                </div>
+              ))}
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <SearchBar/>
-                    </li>
+          </li>
 
-                    <li>
-                            <Link to="/user-registration">Rekisteröidy</Link>
-                    </li>
-
-                    <li className="dropdown">
-                        <span className="material-icons-outlined">menu</span>
-                        <div className="dropdown-content">
-                            {navlinks.map(({to, label}) => (
-                                <div key={to}>
-                                    <Link to={to}>{label}</Link>
-                                </div>
-                            ))}
-                        </div>
-                    </li>
-
-                    <li>
-                        <span className="material-icons-outlined">account_circle</span>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+          <li className="dropdown">
+            <span className="material-icons-outlined">account_circle</span>
+            <div className="dropdown-content">
+              <div
+                className="logout"
+                onClick={() => {
+                  onLogoutClick();
+                }}>
+                Log out
+              </div>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
