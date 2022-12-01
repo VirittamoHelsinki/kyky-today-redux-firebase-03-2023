126b2db5c2a636c05473758c0d82a60e1c0fede6
Nicholas Mononen <nicholas.mononen@gmail.com>
2022-12-01 14:10:34 +0200

    step 5 changes

--- kyky/src/pages/ProfileCreation/components/Step5.js
+++ kyky/src/pages/ProfileCreation/components/Step5.js
@@ -32,6 +32,50 @@ export default function Step5() {
     { value: 'chimneyOrGutterMaintenance', label: 'Chimney / Gutter Maintenance' }
   ];
 
+  const subCategory2 = [
+    { value: 'movingServices', label: 'Moving Services' },
+    { value: 'cleaningServices', label: 'Cleaning Services' },
+    { value: 'carpetCleaning', label: 'Carpet Cleaning' }
+  ];
+
+  const subCategory3 = [
+    { value: 'babysitting', label: 'Babysitting' },
+    { value: 'nurse', label: 'Nurse' },
+    { value: 'eldelryCare', label: 'Eldelry Care' },
+    { value: 'yoga', label: 'Yoga' },
+    { value: 'dietitian', label: 'Dietitian' },
+    { value: 'meditation', label: 'Meditation' },
+    { value: 'sportsAndFitnessTrainer', label: 'Sports & Fitness trainer' },
+    { value: 'massageTherapist', label: 'MassageTherapist' },
+    { value: 'fitnessCoach', label: 'Fitness Coach' }
+  ];
+
+  const subCategory4 = [
+    { value: 'photographer', label: 'Photographer' },
+    { value: 'graphicDesigner', label: 'Graphic Designer' },
+    { value: 'contentWriter', label: 'ContentWriter' },
+    { value: 'translators', label: 'Translators' },
+    { value: 'artAndCrafts', label: 'Art & Crafts' },
+    { value: 'music', label: 'Music' },
+    { value: 'itDeveloper', label: 'IT Developer' },
+    { value: 'itDesigner', label: 'IT Designer' },
+    { value: 'programmer', label: 'Programmer' },
+    { value: 'technicians', label: 'Technicians' },
+    { value: 'socialMedia', label: 'Social Media' }
+  ];
+
+  const subCategory5 = [
+    { value: 'teachersAndTutors', label: 'Teachers & Tutors' },
+    { value: 'lifeCoach', label: 'Life Coach' },
+    { value: 'virtualAssistent', label: 'Virtual Assistent' },
+    { value: 'accountant', label: 'Accountant' },
+    { value: 'businessConsultant', label: 'Business Consultant' },
+    { value: 'lawyer', label: 'Lawyer' },
+    { value: 'itDeveloper', label: 'IT Developer' },
+    { value: 'taxAndFinanceConsultant', label: 'Tax & Finance Consultant' },
+    { value: 'architect', label: 'Architect' }
+  ];
+
   return (
     <div className="step step5">
       <GenericSelect
