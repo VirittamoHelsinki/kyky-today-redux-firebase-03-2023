ee32fcb06823829c55c6bed44343c5cc7ed90a61
Ina Andersin <andeina@digitalentshelsinki.internal>
2022-12-07 15:22:04 +0200

    checkbox, select and visibility

--- kyky/src/pages/ServiceBooking.js
+++ kyky/src/pages/ServiceBooking.js
@@ -2,6 +2,8 @@ import dogPic from '../image/martin-dalsgaard-sGV1QDMM0Gg-unsplash.jpg';
 import {useState} from "react";
 import Button from "../components/Button";
 import Calendar from "../components/Calendar";
+import Checkbox from "../components/Checkbox";
+import Select from "react-select";
 
 const Tabs = {
     Once: "Once",
@@ -10,7 +12,28 @@ const Tabs = {
 
 function ServiceBooking() {
     const [currentTab, setCurrentTab] = useState(Tabs.Once);
+    const [isChecked, setIsChecked] = useState(false);
+    const [weekday, setWeekday] = useState();
     const [date, setDate] = useState(new Date());
+
+    function checkboxState() {
+        if (isChecked) {
+            setIsChecked(false);
+        } else {
+            setIsChecked(true);
+        }
+    }
+
+    const options = [
+        {value: 'monday', label: 'Monday'},
+        {value: 'tuesday', label: 'Tuesday'},
+        {value: 'wednesday', label: 'Wednesday'},
+        {value: 'thursday', label: 'Thursday'},
+        {value: 'friday', label: 'Friday'},
+        {value: 'saturday', label: 'Saturday'},
+        {value: 'sunday', label: 'Sunday'}
+    ];
+
 
     return (
         <>
@@ -24,10 +47,22 @@ function ServiceBooking() {
                 <div className="service-booking-right-panel">
                     <div className="service-booking-right-panel-content">
                         <div className="service-booking-right-panel-buttons">
-                            <div className={`panel-button ${currentTab === Tabs.Once ? 'selected' : ''}`} onClick={()=> setCurrentTab(Tabs.Once)}>Once</div>
-                            <div className={`panel-button ${currentTab === Tabs.Recurring ? 'selected' : ''}`} onClick={()=> setCurrentTab(Tabs.Recurring)}>Recurring</div>
+                            <div className={`panel-button ${currentTab === Tabs.Once ? 'selected' : ''}`}
+                                 onClick={() => setCurrentTab(Tabs.Once)}>Once
+                            </div>
+                            <div className={`panel-button ${currentTab === Tabs.Recurring ? 'selected' : ''}`}
+                                 onClick={() => setCurrentTab(Tabs.Recurring)}>Recurring
+                            </div>
                         </div>
-                        <Calendar date={date} setDate={setDate} minYears={0} maxYears={5}/>
+                        {currentTab === Tabs.Once &&
+                            <Calendar date={date} setDate={setDate} minYears={0} maxYears={5}/>}
+                        {currentTab === Tabs.Recurring && <div className="recurring-tab-container">
+                            <div className="checkbox-and-price"><Checkbox label="Every weekday" className="checkbox"
+                                                                          onChange={() => checkboxState()}/>
+                                <h3>25€/h</h3>
+                                <div>{isChecked && <Select options={options} onChange={(e)=> console.log(e)}/>}</div>
+                            </div>
+                        </div>}
                     </div>
                     <Button children={<div>Continue</div>}/>
                 </div>
