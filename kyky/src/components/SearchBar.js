7e5ec7cad54477f7c23999685285eb0efda70dd1
lehmeet <lehmus.eetu@gmail.com>
2022-12-14 13:47:33 +0200

    search-bar-styling-moved

--- kyky/src/components/SearchBar.js
+++ kyky/src/components/SearchBar.js
@@ -1,40 +1,18 @@
 import React from 'react';
+import '../styles/SearchBar.scss';
+import { ReactComponent as Xiaoyuan } from '../image/xiaoyuan-search.svg';
 
 const SearchBar = ({ keyword, setKeyword }) => {
-  const searchStyling = {
-    display: 'flex',
-    alignItems: 'stretch'
-  };
-
-  const barStyling = {
-    width: '20rem',
-    borderWidth: '0.5px',
-    borderColor: '#808080',
-    borderRadius: '8px 0 0 8px'
-  };
-
-  const btnStyling = {
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: '#063831',
-    color: 'white',
-    borderRadius: '0 8px 8px 0',
-    height: '42px',
-    width: '42px'
-  };
-
   return (
-    <div style={searchStyling}>
+    <div className="searchStyling">
       <input
-        style={barStyling}
+        className="barStyling"
         key="random1"
         value={keyword}
         placeholder={'search...'}
         onChange={(e) => setKeyword(e.target.value)}
       />
-      <span className="material-icons-outlined" style={btnStyling}>
-        search
-      </span>
+      <Xiaoyuan className="btnStyling" />
     </div>
   );
 };
