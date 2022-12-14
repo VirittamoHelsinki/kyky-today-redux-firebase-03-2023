c52df0805265e74e9a7b10aef599522d5bbc73b3
lehmeet <lehmus.eetu@gmail.com>
2022-12-14 10:07:57 +0200

    moved-images-from-public-to-src-and-fixes

--- kyky/src/pages/LandingPage.js
+++ kyky/src/pages/LandingPage.js
@@ -1,31 +1,37 @@
-import {useState} from "react";
+import { useState } from 'react';
+import dogIMG from '../image/dog-walker.png';
+import celebrationIMG from '../image/celebration.png';
+import cookingIMG from '../image/cooking.png';
+import technologyIMG from '../image/technology.png';
+import gardeningIMG from '../image/gardening.png';
+import photographyIMG from '../image/photography.png';
+import teachingIMG from '../image/teaching.png';
 
 function LandingPage() {
-
-    const [landingPageImageClass, setLandingPageImageClass] = useState('image1');
+  const [landingPageImage, setLandingPageImage] = useState(cookingIMG);
 
   return (
     <div className="landing-page-container">
-        <div className="landing-page-nav-and-image-container">
-            <div className="landing-page-navigation-container">
-                            <button onMouseEnter={()=>setLandingPageImageClass('image1')}>Koti & Korjaukset</button>
-                            <button onMouseEnter={()=>setLandingPageImageClass('image2')}>Kauneus & Muoti</button>
-                            <button onMouseEnter={()=>setLandingPageImageClass('image3')}>Koulutus & Kielet</button>
-                            <button onMouseEnter={()=>setLandingPageImageClass('image4')}>Hyvinvointi</button>
-                            <button onMouseEnter={()=>setLandingPageImageClass('image5')}>Ruoka & Tapahtumat</button>
-                            <button onMouseEnter={()=>setLandingPageImageClass('image6')}>Lemmikit</button>
-                            <button onMouseEnter={()=>setLandingPageImageClass('image7')}>Luovuus & IT</button>
-                            <button onMouseEnter={()=>setLandingPageImageClass('image7')}>Konsultointi</button>
-            </div>
-            <div className="landing-page-image">
-                <h1>Let's get things done!</h1>
-                <div className={`${landingPageImageClass}`}/>
-            </div>
+      <div className="landing-page-nav-and-image-container">
+        <div className="landing-page-navigation-container">
+          <button onMouseEnter={() => setLandingPageImage(cookingIMG)}>Koti & Korjaukset</button>
+          <button onMouseEnter={() => setLandingPageImage(photographyIMG)}>Kauneus & Muoti</button>
+          <button onMouseEnter={() => setLandingPageImage(teachingIMG)}>Koulutus & Kielet</button>
+          <button onMouseEnter={() => setLandingPageImage(celebrationIMG)}>Hyvinvointi</button>
+          <button onMouseEnter={() => setLandingPageImage(gardeningIMG)}>Ruoka & Tapahtumat</button>
+          <button onMouseEnter={() => setLandingPageImage(dogIMG)}>Lemmikit</button>
+          <button onMouseEnter={() => setLandingPageImage(technologyIMG)}>Luovuus & IT</button>
+          <button onMouseEnter={() => setLandingPageImage(celebrationIMG)}>Konsultointi</button>
         </div>
-        <div className="landing-page-footer">
-            <button>Just Browsing?</button>
-            <button>Join Us!</button>
+        <div className="landing-page-image">
+          <h1>Let's get things done!</h1>
+          <img src={landingPageImage} />
         </div>
+      </div>
+      <div className="landing-page-footer">
+        <button>Just Browsing?</button>
+        <button>Join Us!</button>
+      </div>
     </div>
   );
 }
