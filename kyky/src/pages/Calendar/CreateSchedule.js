dbbcdb2c4115d354859720a0513fbe65152dac9e
Mikko Manninen <mannmik@digitalentshelsinki.internal>
2022-12-20 11:00:31 +0200

    extension bug fixed

--- kyky/src/pages/Calendar/CreateSchedule.js
+++ kyky/src/pages/Calendar/CreateSchedule.js
@@ -95,7 +95,7 @@ export default function ManageScheduleModal({ setScheduleWindow, editing }) {
   }
 
   function SubmitDetails() {
-    const schedules = _schedules[properties.jobId + '_schedules'];
+    const schedules = JSON.parse(JSON.stringify(_schedules[properties.jobId + '_schedules']));
     if (mode === 'create') {
       const id = Math.random().toString(36).substring(2, 9); // generate random id
       const data = { ...properties, _id: id };
