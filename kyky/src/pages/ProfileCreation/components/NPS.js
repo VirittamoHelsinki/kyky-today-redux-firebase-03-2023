29d6fe14c12b978d081b4a6a5ad345ffafec1361
Nicholas Mononen <nicholas.mononen@gmail.com>
2023-01-03 15:09:12 +0200

    update buyers rating

--- kyky/src/pages/ProfileCreation/components/NPS.js
+++ kyky/src/pages/ProfileCreation/components/NPS.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, React } from 'react';
 import Button from '../../../components/Button';
 import { ReactComponent as Happy } from '../../../image/npsHappy.svg';
 import { ReactComponent as VeryHappy } from '../../../image/npsVeryHappy.svg';
@@ -8,15 +8,41 @@ import { ReactComponent as VerySad } from '../../../image/npsVerySad.svg';
 import '../../../styles/buyersrating.scss';
 
 export default function NPS() {
+  const [verySad, setVerySad] = useState('emojiAttributes1');
+  const [sad, setSad] = useState('emojiAttributes1');
+  const [neutral, setNeutral] = useState('emojiAttributes1');
+  const [happy, setHappy] = useState('emojiAttributes1');
+  const [veryHappy, setVeryHappy] = useState('emojiAttributes1');
+
+  const selectVerySad = () => {
+    setVerySad('emojiAttributes2');
+  };
+  const selectSad = () => {
+    setSad('emojiAttributes2');
+  };
+  const selectNeutral = () => {
+    setNeutral('emojiAttributes2');
+  };
+  const selectHappy = () => {
+    setHappy('emojiAttributes2');
+  };
+  const selectVeryHappy = () => {
+    setVeryHappy('emojiAttributes2');
+  };
   return (
-    <div className= "nps">
-      <div className= "npsMainContainer">
+    <div className="nps">
+      <div className="npsMainContainer">
         <h1>How happy are you with this service?</h1>
-        <div className= "emojiContainer"><VerySad /><Sad /><Neutral /><Happy/>
-        <VeryHappy /></div>
-    <Button className="submitButton" link="nps">
-      Submit
-    </Button>
+        <div className="emojiContainer">
+          <VerySad className={verySad} onClick={selectVerySad} />
+          <Sad className={sad} onClick={selectSad} />
+          <Neutral className={neutral} onClick={selectNeutral} />
+          <Happy className={happy} onClick={selectHappy} />
+          <VeryHappy className={veryHappy} onClick={selectVeryHappy} />
+        </div>
+        <Button className="submitButton" link="nps">
+          Submit
+        </Button>
       </div>
     </div>
   );
