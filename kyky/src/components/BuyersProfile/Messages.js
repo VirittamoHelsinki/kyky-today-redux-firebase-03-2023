1bc60255f0ec76e50d0b2b7bec067e4fef670686
Mikko Manninen <mikkomanninen79@gmail.com>
2023-02-01 07:00:11 +0200

    message content start

---
+++ kyky/src/components/BuyersProfile/Messages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/BuyersProfile.scss';
+
+const Messages = () => {
+  return (
+    <div className="messages-main">
+      <div className="contacts-content"></div>
+      <div className="messages-content"></div>
+    </div>
+  );
+};
+
+export default Messages;
