b722e71bd39ef8ca5b57023a483a665642a4b2d8
Mikko Manninen <mannmik@digitalentshelsinki.internal>
2023-02-01 15:18:55 +0200

    message mock and styles

--- kyky/src/components/BuyersProfile/Messages.js
+++ kyky/src/components/BuyersProfile/Messages.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
+import contacts from '../../mock_chats.json';
 import '../../styles/BuyersProfile.scss';
 
-const Messages = () => {
+const Messages = ({ user }) => {
+  const [selectedContact, setSelectedContact] = useState([]);
+
   return (
     <div className="messages-main">
-      <div className="contacts-content"></div>
-      <div className="messages-content"></div>
+      <div className="contacts-content">
+        <div className="contact-list">
+          {contacts.map((c, index) => (
+            <div
+              className="contact"
+              key={index}
+              onClick={() => {
+                setSelectedContact(c.messages);
+              }}>
+              <div className="contact-left-side">
+                <img
+                  src="https://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=mp"
+                  className="profile-img"
+                  referrerPolicy="no-referrer"
+                  alt=""
+                />
+                <div className="name-latest-message-paragraph">
+                  <p className="name-paragraph">Emerson Fittipaldi</p>
+                  <p className="latest-message-paragraph">ok done..</p>
+                </div>
+              </div>
+              <div className="contact-right-side">
+                <p className="time-since-last-message">1 hr</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="messages-content">
+        <div className="messages-list">
+          {selectedContact.map((m) => (
+            <div className={`message-${user.uid === m.userid ? 'myself' : 'friend'}`}>
+              <div className="message-box">{m.message}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- kyky/src/pages/BuyersProfile.js
+++ kyky/src/pages/BuyersProfile.js
@@ -1,33 +1,25 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import '../styles/BuyersProfile.scss';
 import Purchases from '../components/BuyersProfile/Purchases';
+import Messages from '../components/BuyersProfile/Messages';
+import '../styles/BuyersProfile.scss';
 
-const Tabs = {
-  Purchases: 'Ostokset',
-  Messages: 'Viestit',
-  Ratings: 'Arvostelut',
-  Settings: 'Tilin asetukset'
-};
+const Tabs = [
+  { tab: 'Purchases', display: 'Ostokset' },
+  { tab: 'Messages', display: 'Viestit' },
+  { tab: 'Ratings', display: 'Arvostelut' },
+  { tab: 'Settings', display: 'Tilin asetukset' }
+];
 
 function BuyersProfile() {
   const [currentTab, setCurrentTab] = useState(Tabs.Purchases);
 
   const _user = useSelector((state) => state.user);
 
-  function selectedTab(currentTab) {
-    setCurrentTab(currentTab);
-  }
-
   return (
     <div className="buyers-profile--container">
       <div className="buyers-profile--header">
-        <img
-          src={_user.photoURL}
-          referrerPolicy="no-referrer"
-          className="userImage"
-          style={{ width: '100px', height: '100px', background: '#387a70', borderRadius: '50px' }}
-        />
+        <img src={_user.photoURL} referrerPolicy="no-referrer" className="userImage" />
         <div>
           <h3 className="username">{_user.displayName}</h3>
         </div>
@@ -35,38 +27,18 @@ function BuyersProfile() {
 
       <div className="buyers-profile--content-wrapper">
         <div className="buyers-profile--tabs">
-          <div
-            className={`buyers-profile--button ${
-              currentTab === Tabs.Purchases ? 'selected-tab' : ''
-            }`}
-            onClick={() => selectedTab(Tabs.Purchases)}>
-            <h3>Ostokset</h3>
-          </div>
-          <div
-            className={`buyers-profile--button ${
-              currentTab === Tabs.Messages ? 'selected-tab' : ''
-            }`}
-            onClick={() => selectedTab(Tabs.Messages)}>
-            <h3>Viestit</h3>
-          </div>
-          <div
-            className={`buyers-profile--button ${
-              currentTab === Tabs.Ratings ? 'selected-tab' : ''
-            }`}
-            onClick={() => selectedTab(Tabs.Ratings)}>
-            <h3>Arvostelut</h3>
-          </div>
-          <div
-            className={`buyers-profile--button ${
-              currentTab === Tabs.Settings ? 'selected-tab' : ''
-            }`}
-            onClick={() => selectedTab(Tabs.Settings)}>
-            <h3>Tilin asetukset</h3>
-          </div>
+          {Tabs.map(({ tab, display }) => (
+            <div
+              className={`buyers-profile--button ${currentTab === tab ? 'selected-tab' : ''}`}
+              onClick={() => setCurrentTab(tab)}>
+              <h3>{display}</h3>
+            </div>
+          ))}
         </div>
 
         <div className="buyers-profile--content">
-          {currentTab === Tabs.Purchases && <Purchases />}
+          {currentTab === 'Purchases' && <Purchases />}
+          {currentTab === 'Messages' && <Messages user={_user} />}
         </div>
       </div>
     </div>
